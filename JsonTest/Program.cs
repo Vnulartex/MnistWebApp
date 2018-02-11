645f2ef4628a0e680f1989ef947cfc7633940f91
Jiří Vlasák <jirivlasak9@gmail.com>
2018-02-11 13:32:43 +0100

    aa

--- JsonTest/Program.cs
+++ JsonTest/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using Accord.Math;
 
 namespace JsonTest
 {
@@ -6,7 +11,29 @@ namespace JsonTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // string netData = new StreamReader("ver1.json").ReadToEnd();
+           //// Net net = JsonConvert.DeserializeObject<Net>(netData);
+           // double[][] activations = new double[net.sizes.Length][];
+           // for (int i = 0; i < activations.Length; i++)
+           // {
+           //     activations[i]=new double[net.sizes[i]];
+           // }
+           // string val=new StreamReader("7.json").ReadToEnd();
+           // Input input = JsonConvert.DeserializeObject<Input>(val);
+           // activations[0] = input.values;
+           // net.FeedForward(activations);
+            
+           // Console.ReadKey();
         }
+
+
+        class Input
+        {
+            public double[] values;
+            public double[] labels;
+        }
+       
+
+        
     }
 }
---
+++ NeuralNet/Net.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Math;
+using Newtonsoft.Json;
+
+namespace NeuralNet
+{
+    class Net
+    {
+        public int[] Sizes { get; }
+        public double[][][] Weights { get; }
+        public double[][][] Biases { get; }
+
+        public static Net Init (string data)
+        {
+            string netData = new StreamReader("ver1.json").ReadToEnd();
+            Net net = JsonConvert.DeserializeObject<Net>(netData);
+            //double[][] activations = new double[net.Sizes.Length][];
+            //for (int i = 0; i < activations.Length; i++)
+            //{
+            //    activations[i]=new double[net.Sizes[i]];
+            //}
+            return net;
+        }
+
+        public double[][] FeedForward(double[][] a)
+        {
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                for (int j = 0; j < Weights[i].Length; j++)
+                {
+                    a[i + 1][j] = Sigmoid(a[i].Dot(Weights[i][j]) + Biases[i][j][0]);
+                }
+            }
+            return a;
+        }
+
+        private double Sigmoid(double z)
+        {
+            return 1.0 / (1.0 + Math.Exp(-z));
+        }
+    }
+}
