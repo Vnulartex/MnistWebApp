32501274f4ba7e3a8d07e8948a8f5369a7e4a686
Jiří Vlasák <jirivlasak9@gmail.com>
2018-02-22 21:58:03 +0100

    fixed normalization

--- NeuralNet/ExtensionBitmap.cs
+++ NeuralNet/ExtensionBitmap.cs
@@ -3,37 +3,45 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 
+
 namespace NeuralNet
 {
     public static class ExtensionBitmap
     {
-        public static Bitmap Resize(this Bitmap origBitmap, int newWidth, int newHeight,int newX=0,int newY=0,int sourceX=0,int sourceY=0,int sourceWidth=0,int sourceHeight=0)
+        public static Bitmap Resize(this Bitmap origBitmap, int canvasWidth, int canvasHeight, int newX = 0, int newY = 0,
+            int sourceX = 0, int sourceY = 0, int sourceWidth = 0, int sourceHeight = 0, int newSourceWidth = 0,
+            int newSourceHeight = 0)
         {
             if (sourceWidth == 0)
                 sourceWidth = origBitmap.Width;
             if (sourceHeight == 0)
                 sourceHeight = origBitmap.Height;
-            Bitmap newBitmap = new Bitmap(newWidth, newHeight);
+            if (newSourceWidth == 0)
+                newSourceWidth = canvasWidth;
+            if (newSourceHeight == 0)
+                newSourceHeight = canvasHeight;
+            Bitmap newBitmap = new Bitmap(canvasWidth, canvasHeight);
             ImageAttributes imageAttributes=new ImageAttributes();
             imageAttributes.SetWrapMode(WrapMode.TileFlipXY);
             using (Graphics gr = Graphics.FromImage(newBitmap))
-            {
+            {         
                 gr.SmoothingMode = SmoothingMode.HighQuality;
                 gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
                 gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
                 gr.CompositingQuality = CompositingQuality.HighQuality;
-                gr.DrawImage(origBitmap, new Rectangle(0,0,newBitmap.Width,newBitmap.Height),sourceX,sourceY,sourceWidth,sourceHeight,GraphicsUnit.Pixel,imageAttributes);
+                gr.Clear(Color.White);
+                gr.DrawImage(origBitmap, new Rectangle(newX,newY,newSourceWidth,newSourceHeight),sourceX,sourceY,sourceWidth,sourceHeight,GraphicsUnit.Pixel,imageAttributes);
             }
             return newBitmap; 
         }
 
         public static double[][] ToArray(this Bitmap imageBitmap, int arraySize)
         {
-            double[][] pixelDoubles=new double[arraySize][];
-            for (int i = 0; i < arraySize; i++)
+            double[][] pixelDoubles=new double[imageBitmap.Height][];
+            for (int i = 0; i < imageBitmap.Height; i++)
             {
-                pixelDoubles[i]=new double[arraySize];
-                for (int j = 0; j < arraySize; j++)
+                pixelDoubles[i]=new double[imageBitmap.Width];
+                for (int j = 0; j < imageBitmap.Width; j++)
                 {
                     pixelDoubles[i][j] = imageBitmap.GetGrayscale(j, i);
                 }
@@ -47,46 +55,44 @@ namespace NeuralNet
             return 1-(float)pixel.R/255;
         }
 
-        private static Rectangle FindBounds(this Bitmap bmp,float threshold)
+        private static Rectangle FindBounds(this Bitmap bmp)
         {
+            int size = bmp.Width;
             Rectangle bounds = new Rectangle();
-            //Left
-            for (int x = 0; x < bmp.Width; x++)
-            {
-                for (int y = 0; y < bmp.Height; y++)
-                {
-                    if (bmp.GetGrayscale(x, y) > threshold)
-                        bounds.X = x;
-                }
-            }
-            //Right
-            for (int x = bmp.Width-1; x > 0; x--)
-            {
-                for (int y = bmp.Height-1; y > 0; y--)
-                {
-                    if (bmp.GetGrayscale(x, y) > threshold)
-                        bounds.Width = x-bounds.X;
-                }
-            }
-            //Top
-            for (int y = bmp.Height-1; y > 0; y--)
-            {
-                for (int x = bmp.Width-1; x > 0; x--)
-                {
-                    if (bmp.GetGrayscale(x, y) > threshold)
-                        bounds.Y = y;
-                }
-            }
-            //Bottom
-            for (int y = 0; y < bmp.Height; y++)
-            {
-                for (int x = 0; x < bmp.Width; x++)
-                {
-                    if (bmp.GetGrayscale(x, y) > threshold)
-                        bounds.Height = bounds.Y-y ;
-                }
-            }
+            bounds.X = bmp.FindBound(true, 0);
+            bounds.Y = bmp.FindBound(false, 0);
+            bounds.Width = bmp.FindBound(true, size-1)-bounds.X + 1;
+            bounds.Height =  bmp.FindBound(false, size-1)-bounds.Y + 1;
             return bounds;
+        }
+
+        public static int FindBound(this Bitmap bmp,bool horizontal,int defVal)
+        {
+            int a = defVal;
+            int b = defVal;
+            int increment = 1;
+            if (defVal!=0)
+            {
+                increment = -1;
+            }
+
+            for (a=defVal; a >= 0 && a < bmp.Width; a += increment)
+            {
+                for (b=defVal; b >= 0 && b < bmp.Width; b += increment)
+                {
+                    if (horizontal)
+                    {
+                        if (bmp.GetGrayscale(a, b) > 0)
+                            return a;
+                    }
+                    else
+                    {
+                        if (bmp.GetGrayscale(b, a) > 0)
+                            return a;
+                    }
+                }
+            }
+            throw new Exception();
         }
 
         public static Bitmap Center(this Bitmap origBmp)
@@ -105,20 +111,20 @@ namespace NeuralNet
 
         public static Bitmap Normalize(this Bitmap bmp)
         {
-            Rectangle bounds = bmp.FindBounds(0);
-            float factor = (float)bounds.Width / bounds.Height;
+            Rectangle bounds = bmp.FindBounds();
+            int canvasWidth = 28;
+            int canvasHeight = 28;
+            float factor = (float) bounds.Width / bounds.Height;
             int newWidth = 20;
             int newHeight = 20;
             if (factor > 1)
-                newHeight = (int)(newWidth / factor);
+                newHeight = (int) (newWidth / factor);
             else if (factor < 1)
-                newWidth = (int) (newHeight / factor);
-            int newX = (20 - newWidth) / 2;
-            int newY = (20 - newHeight) / 2;
-            bmp = bmp.Resize(newWidth, newHeight,newX,newY, bounds.X, bounds.Y, bounds.Width, bounds.Height);//vycentrovat ve 20x20 ramecku
-            newX = (28 - 20) / 2;
-            newY = (28 - 20) / 2;    
-            return bmp.Resize(28, 28, newX, newY);
+                newWidth = (int) (newHeight * factor);
+            int newX = (canvasWidth - newWidth) / 2;
+            int newY = (canvasHeight - newHeight) / 2;
+            return bmp.Resize(canvasWidth, canvasHeight, newX, newY, bounds.X, bounds.Y, bounds.Width, bounds.Height,
+                newWidth, newHeight); //scalovat v 20x20 ramecku a vložit do středu 28x28 ramecku
 
         }
     }
